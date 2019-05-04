b454675cc71b5c30d1ae9fb6c3a1b887a32e1e45
Jason Wong <jason.hcwong@gmail.com>
2019-05-04 12:03:08 +0000

    restructure manager

--- manager_entrypoint.sh
+++ manager_entrypoint.sh
@@ -6,12 +6,10 @@ memLimit_1gb="1508344"
 memLimit_2gb="3016688"
 memTotal=`grep MemTotal /proc/meminfo | awk '{print $2}'`
 
-function update_yml_and_config() {
-  git pull
-
+function update_monerobox() {
   # copy dockercompose yml files
-  cp settings/*.yml /settings
-  
+  cp settings/monerobox.yml /settings
+
   # copy service json only if it does not exist
   if [ ! -f "/settings/service.json" ]; then
     cp settings/service.json /settings/
@@ -19,6 +17,7 @@ function update_yml_and_config() {
     echo "service json already exist."
     # add data to service json
   fi
+
   # copy tor config file only if it does not exist
   if [ ! -f "/settings/tor.conf" ]; then
     cp settings/tor.conf /settings/
@@ -26,6 +25,7 @@ function update_yml_and_config() {
     echo "tor config already exist."
     # add data to tor config
   fi
+
   # copy stunnel config file only if it does not exist
   if [ ! -f "/settings/stunnel.conf" ]; then
     cp settings/stunnel.conf /settings/
@@ -34,16 +34,44 @@ function update_yml_and_config() {
     # add data to stunnel config
   fi
 
+  # create volume if they do not exist
+  docker volume create data_tor
 
+  # pull and start mandatory docker images
+  /usr/local/bin/docker-compose -f /settings/monerobox.yml pull
+  /usr/local/bin/docker-compose -f /settings/monerobox.yml up -d tor
+  /usr/local/bin/docker-compose -f /settings/monerobox.yml up -d web
+  # manager container does not start itself, this is done by web container
+}
 
-  # copy config file according to RAM size of the board, only if it does not exist
-  # Monero related service for every model
+function update_monero() {
+  # copy dockercompose yml files
+  cp settings/monero.yml /settings
+
+  # copy config file
   if [ ! -f "/settings/monerod.conf" ]; then
     cp settings/monerod.conf /settings
   fi
-  
+
+  # create volume if they do not exist
+  docker volume create data_monero
+
+  # pull and start optional docker images
+  enabled=$(jq '.monero.enabled' /settings/service.json)
+  if [ "$enabled" = "true" ]; then
+    /usr/local/bin/docker-compose -f /settings/monero.yml pull
+    /usr/local/bin/docker-compose -f /settings/monero.yml up -d
+  fi
+}
+
+
+function update_litecoin() {
   # Litecoin related service for models with 2GB and 4GB RAM
   if [ "$memTotal" -gt "$memLimit_1gb" ]; then
+    # copy dockercompose yml files
+    cp settings/litecoin.yml /settings
+
+    # copy config file
     if [ ! -f "/settings/litecoind.conf" ]; then
       cp settings/litecoind.conf /settings
     fi
@@ -54,10 +82,27 @@ function update_yml_and_config() {
       mkdir /settings/rtl_ltc
       cp settings/rtl_ltc.conf /settings/rtl_ltc/RTL.conf
     fi
+
+    # create volumes if they don't exist
+    docker volume create data_litecoin
+    docker volume create data_lnd_ltc
+
+    # pull and start optional docker images
+    enabled=$(jq '.litecoin.enabled' /settings/service.json)
+    if [ "$enabled" = "true" ]; then
+      /usr/local/bin/docker-compose -f /settings/litecoin.yml pull
+      /usr/local/bin/docker-compose -f /settings/litecoin.yml up -d
+    fi
   fi
-  
+}
+
+function update_bitcoin() {
   # Bitcoin related service for models 4GB RAM
   if [ "$memTotal" -gt "$memLimit_2gb" ]; then
+    # copy dockercompose yml files
+    cp settings/bitcoin.yml /settings
+
+    # copy config files
     if [ ! -f "/settings/bitcoind.conf" ]; then
       cp settings/bitcoind.conf /settings
     fi
@@ -68,55 +113,28 @@ function update_yml_and_config() {
       mkdir /settings/rtl_btc
       cp settings/rtl_btc.conf /settings/rtl_btc/RTL.conf
     fi
+
+    # create volumes if they don't exist
+    docker volume create data_bitcoin
+    docker volume create data_lnd_btc
+
+    # pull and start optional docker images
+    enabled=$(jq '.bitcoin.enabled' /settings/service.json)
+    if [ "$enabled" = "true" ]; then
+      /usr/local/bin/docker-compose -f /settings/bitcoin.yml pull
+      /usr/local/bin/docker-compose -f /settings/bitcoin.yml up -d
+    fi
+
   fi
 }
 
-function extra_docker_commands() {
-  # create volumes if they don't exist
-  docker volume create data_tor
-  docker volume create data_monero
-  docker volume create data_litecoin
-  docker volume create data_bitcoin
-  docker volume create data_lnd_ltc
-  docker volume create data_lnd_btc
-}
-
-function update_docker_images() {
-  # pull and start optional docker images
-  enabled=$(jq '.monero.enabled' /settings/service.json)
-  if [ "$enabled" = "true" ]; then
-    /usr/local/bin/docker-compose -f /settings/monero.yml pull
-    /usr/local/bin/docker-compose -f /settings/monero.yml up -d
-  fi
-  enabled=$(jq '.litecoin.enabled' /settings/service.json)
-  if [ "$enabled" = "true" ]; then
-    /usr/local/bin/docker-compose -f /settings/litecoin.yml pull
-    /usr/local/bin/docker-compose -f /settings/litecoin.yml up -d
-  fi
-  enabled=$(jq '.bitcoin.enabled' /settings/service.json)
-  if [ "$enabled" = "true" ]; then
-    /usr/local/bin/docker-compose -f /settings/bitcoin.yml pull
-    /usr/local/bin/docker-compose -f /settings/bitcoin.yml up -d
-  fi
-
-  # pull and start mandatory docker images
-  /usr/local/bin/docker-compose -f /settings/monerobox.yml pull
-  /usr/local/bin/docker-compose -f /settings/monerobox.yml up -d tor
-  /usr/local/bin/docker-compose -f /settings/monerobox.yml up -d web
-  # manager container does not start itself, this is done by web container
-}
 
 cd /usr/src/app/monerobox
+git pull
 
-while :
-do
-  update_yml_and_config
+update_monerobox
+update_monero
+update_litecoin
+update_bitcoin
 
-  extra_docker_commands
-
-  update_docker_images
-
-  # check for update every 12 hours
-  sleep 12h
-done
-
+exit
--- start_manager.sh
+++ start_manager.sh
@@ -2,12 +2,6 @@
 
 docker network create --subnet 172.20.1.0/24 --gateway 172.20.1.1 monerobox
 docker volume create settings
-docker volume create data_tor
-docker volume create data_monero
-docker volume create data_litecoin
-docker volume create data_bitcoin
-docker volume create data_lnd_ltc
-docker volume create data_lnd_btc
 
 export HOST_HOSTNAME=$(hostname)
 export HOST_IP=$(ip -4 addr show eth0 | grep -Po 'inet \K[\d.]+')
